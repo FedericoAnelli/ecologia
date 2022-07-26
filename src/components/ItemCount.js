a9fff616156ca5dd15b7cbb85a31c6f50a635e45
Federico Anelli <federicoanelli@gmail.com>
2022-07-26 11:25:51 -0300

    Minor fixes

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,26 +3,30 @@ import { useEffect, useState } from "react";
 
 function ItemCount ({stockDisponible, stockInicial, onAdd}) {
     let initial = parseInt(stockInicial);
-    const [name, setName] = useState(initial);
+    const [contador, setContador] = useState(initial);
 
     const addOne = () => {
-        if(name < parseInt(stockDisponible)){
-            setName(name+1);
+        if(contador < parseInt(stockDisponible)){
+            setContador(contador+1);
         }
     }
 
     const substractOne = () => {
-        if (name > parseInt(stockInicial)){
-            setName(name-1);
+        if (contador > parseInt(stockInicial)){
+            setContador(contador-1);
         }
     }
 
     return (
-        
         <div>
-            <button className='contadorButton' type='button' onClick={substractOne}>-</button>
-            <input className='contadorInput' type="number" value={name}></input>
-            <button className='contadorButton' type='button' onClick={addOne}>+</button>
+            <div>
+                <button className='contadorButton' type='button' onClick={substractOne}>-</button>
+                <input className='contadorInput' type="number" value={contador}></input>
+                <button className='contadorButton' type='button' onClick={addOne}>+</button>
+            </div>
+            <div>
+                <button className='agregarAlCarritoButton' type='button' onClick={()=>onAdd(contador)}> AGREGAR AL CARRITO </button>
+            </div>
         </div>
 
     );
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,15 @@
 import ItemCount from './ItemCount';
 import './ItemListContainer.css';
 
+const onAdd = (cantidad) => {
+    console.log(`Se agregaron ${cantidad} items en el carrito`)
+}
+
 const ItemListContainer = ({greeting}) => {
     return (
         <div>
         <h1>{greeting}</h1>
-        <ItemCount stockDisponible='5' stockInicial='1' onAdd='0' />
+        <ItemCount stockDisponible='5' stockInicial='1' onAdd={onAdd} />
         </div>
     );
 
