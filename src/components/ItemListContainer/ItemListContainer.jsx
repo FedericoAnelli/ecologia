b9c879adc3b8706c1d8c4252765fe2c589330895
Federico Anelli <federicoanelli@gmail.com>
2022-07-26 19:57:50 -0300

    Wip

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,11 +1,36 @@
 import ItemCount from '../ItemCount/ItemCount';
 import './ItemListContainer';
-
-const onAdd = (amount) => {
-    console.log(`Se agregaron ${amount} items en el carrito`)
-}
+import rawData from '../assets/initialConfig.json';
 
 const ItemListContainer = ({greeting}) => {
+    const [listaProyectos, setProyectos]= useState([];)
+    const onAdd = (amount) => {
+        console.log(`Se agregaron ${amount} items en el carrito`)
+    }
+    
+    const data = new Promise((resolve, reject) => {
+    
+        let condition = true;
+        setTimeout(() => {
+            if(condition){
+                resolve(rawData)
+            }else{
+                reject('Salio mal')
+            }
+        }, 3000);
+    
+    })
+    /*
+    useEffect(() => {
+        data
+            .then((response)=> console.log('respuesta', response))
+    
+    
+    }, []);
+*/
+
+
+
     return (
         <div>
         <h1>{greeting}</h1>
