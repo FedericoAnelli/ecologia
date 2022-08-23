1ba5a028108286f16e5f6902fedb3d395f0dde77
Federico Anelli <federicoanelli@gmail.com>
2022-08-23 17:29:04 -0300

    Purchase login

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -2,6 +2,7 @@ import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import { CartContext } from "../../context/CartContext";
+import { UserContext } from '../../context/UserContext';
 import Cart from "../Cart/Cart";
 import "./CartContainer.css";
 import { useNavigate } from 'react-router-dom';
@@ -11,6 +12,7 @@ import Swal from 'sweetalert2';
 const CartContainer = () => {
     const valueToShare = useContext(CartContext)
     const { cart } = useContext(CartContext);
+    const { user } = useContext(UserContext);
     const navigate = useNavigate();
     
     const total = cart.reduce((acc, item) => acc + item.donacion * item.quantity, 0)
@@ -43,25 +45,38 @@ const CartContainer = () => {
             })
     }
 
-
-    
     const handleSendOrder = () => {
-        Swal.fire({
-            title: '<p class="titleAlert">Datos de la compra</p>',
-            html: '<div><p class="inputTexts">Nombre</p>' +
-            '<input id="swal-input1" class="inputField" type="text" placeholder="Ingresar nombre">' +
-            '<div><p class="inputTexts">Teléfono</p>' +
-            '<input id="swal-input2" class="inputField" type="tel" placeholder="Ingresar teléfono">' +
-            '<div><p class="inputTexts">Email</p>' +
-            '<input id="swal-input3" class="inputField" type="email" placeholder="Ingresar mail"></div>',
-            showCancelButton: true,
-            confirmButtonText: '<p class="inputTexts">Enviar</p>',
-            showLoaderOnConfirm: true,
-            focusConfirm: false,
-            preConfirm: () => {
-              sendOrder(document.getElementById('swal-input1').value, document.getElementById('swal-input2').value, document.getElementById('swal-input3').value)
-            }
-          })
+        if(user.length === 0){
+                Swal.fire({
+                    title: '<p class="titleAlert">Datos de la compra</p>',
+                    html: '<div><p class="inputTexts">Nombre</p>' +
+                    '<input id="swal-input1" class="inputField" type="text" placeholder="Ingresar nombre">' +
+                    '<div><p class="inputTexts">Teléfono</p>' +
+                    '<input id="swal-input2" class="inputField" type="tel" placeholder="Ingresar teléfono">' +
+                    '<div><p class="inputTexts">Email</p>' +
+                    '<input id="swal-input3" class="inputField" type="email" placeholder="Ingresar mail"></div>',
+                    showCancelButton: true,
+                    confirmButtonText: '<p class="inputTexts">Enviar</p>',
+                    showLoaderOnConfirm: true,
+                    focusConfirm: false,
+                    preConfirm: () => {
+                    sendOrder(document.getElementById('swal-input1').value, document.getElementById('swal-input2').value, document.getElementById('swal-input3').value)
+                    }
+          })}
+          else
+          {
+                Swal.fire({
+                    title: '<p class="titleAlert">Datos de la compra</p>',
+                    html: '<div><p class="inputTexts"> Comprar como: ' + user.email +'</p></div>',
+                    showCancelButton: true,
+                    confirmButtonText: '<p class="inputTexts">Enviar</p>',
+                    showLoaderOnConfirm: true,
+                    focusConfirm: false,
+                    preConfirm: () => {
+                    sendOrder(user.email, "defaultNumber", "defaultNumber")
+                    }
+                });     
+          }
     }
 
     return (
