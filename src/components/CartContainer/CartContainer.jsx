4b67fbf1542efc0a1f749779cdb0cd9a5635e7db
Federico Anelli <federicoanelli@gmail.com>
2022-08-22 21:58:10 -0300

    Added sweetalert confirmation prompt

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -5,6 +5,7 @@ import { CartContext } from "../../context/CartContext";
 import Cart from "../Cart/Cart";
 import "./CartContainer.css";
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const USER_DEMO = {
     name: "fede",
@@ -26,9 +27,11 @@ const CartContainer = () => {
         navigate('/')
     }
 
-    const sendOrder = () => {
+    const sendOrder = (name, phone, email) => {
         const newOrder = {
-            buyer: USER_DEMO,
+            buyer: name,
+            phone: phone,
+            email: email,
             items: cart,
             total: total 
         }
@@ -37,8 +40,32 @@ const CartContainer = () => {
         const ordersCollection = collection(db, 'orders');
         addDoc(ordersCollection, newOrder)
             .then(({id}) => {
-                console.log('order added', id);
+                Swal.fire({
+                    title: '¡Gracias!',
+                    html: '<p>Su pedido ha sido enviado.<br> Número de órden: ' + id+ '</p>',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                })
+                valueToShare.cleanCart();
             })
+    }
+
+
+    
+    const handleSendOrder = () => {
+        Swal.fire({
+            html: 
+            '<input id="swal-input1" class="swal2-input" placeholder="Ingresar nombre">' +
+            '<input id="swal-input2" class="swal2-input" placeholder="Ingresar teléfono">' +
+            '<input id="swal-input3" class="swal2-input" placeholder="Ingresar mail">',
+            showCancelButton: true,
+            confirmButtonText: 'Enviar',
+            showLoaderOnConfirm: true,
+            focusConfirm: false,
+            preConfirm: () => {
+              sendOrder(document.getElementById('swal-input1').value, document.getElementById('swal-input2').value, document.getElementById('swal-input3').value)
+            }
+          })
     }
 
     return (
@@ -48,7 +75,7 @@ const CartContainer = () => {
             <div className="cartContainer">        
             <Cart items={cart} onDelete={handleDelete}/>
             <p className='totalP'><strong>Total:</strong> ${total}</p>
-            <button className='finalizeButton' onClick={sendOrder}>Finalizar compra</button>
+            <button className='finalizeButton' onClick={handleSendOrder}>Finalizar compra</button>
             </div> 
         ) : (<div className="cartContainer"><p>El carrito está vacío. Click <strong onClick={navigateToHome}>ACÁ</strong> para volver.</p></div>)}
 
